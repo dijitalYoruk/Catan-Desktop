357734770d9f02d03ca2b39430ceed0e1616bb95
fatihsevban <fatihsevban15@gmail.com>
2019-10-27 19:17:36 +0300

    fatih-development: - building sequential constructions are hindered - added settlement model - added new assets - added neighbors property to vertex.

--- src/com/catan/modal/Vertex.java
+++ src/com/catan/modal/Vertex.java
@@ -2,12 +2,24 @@ package com.catan.modal;
 
 import javafx.scene.shape.Circle;
 
+import java.util.ArrayList;
+
 public class Vertex {
 
     private Circle shape;
+    private ArrayList<Vertex> neighbors;
 
     public Vertex(Circle shape) {
+        neighbors = new ArrayList<>();
         this.shape = shape;
+    }
+
+    public ArrayList<Vertex> getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(ArrayList<Vertex> neighbors) {
+        this.neighbors = neighbors;
     }
 
     public Circle getShape() {
@@ -17,4 +29,9 @@ public class Vertex {
     public void setShape(Circle shape) {
         this.shape = shape;
     }
+
+    public void addNeighbor(Vertex vertex) {
+        neighbors.add(vertex);
+    }
+
 }
---
+++ src/com/catan/modal/Village.java
@@ -0,0 +1,7 @@
+package com.catan.modal;
+
+public class Village extends Settlement {
+    public Village(String imagePath, Vertex vertex) {
+        super(imagePath, vertex);
+    }
+}
