1bc2b8518b92b97a732a749209e09caed3bf4a5e
cagrigungor <54181614+cagrigungor@users.noreply.github.com>
2019-10-28 02:32:05 +0300

    cloned unstable

---
+++ src/com/catan/modal/City.java
@@ -0,0 +1,7 @@
+package com.catan.modal;
+
+public class City extends Settlement {
+    public City(String imagePath, Vertex vertex) {
+        super(imagePath, vertex);
+    }
+}
---
+++ src/com/catan/modal/Civilisation.java
@@ -0,0 +1,7 @@
+package com.catan.modal;
+
+public class Civilisation extends Settlement {
+    public Civilisation(String imagePath, Vertex vertex) {
+        super(imagePath, vertex);
+    }
+}
